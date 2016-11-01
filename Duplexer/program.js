012d934eb11a03f913ea162c58c3f021490fa9e3
Andrii Barabash <andrew.barbsh@gmail.com>
2016-11-01 22:47:36 +0200

    Added 'Duplexer'

---
+++ Duplexer/program.js
@@ -0,0 +1,9 @@
+var spawn = require('child_process').spawn;
+var duplex = require('duplexer2');
+
+module.exports = function (cmd, args) {
+	
+	var proc = spawn(cmd, args);
+	return duplex(proc.stdin, proc.stdout);
+
+};
